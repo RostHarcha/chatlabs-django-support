cebc7da5c2c9e5647479a74ce1a6b54ea348c3d4
0TKA3 <lal4topol@yandex.ru>
2025-02-07 22:07:47 +0300

    Revert "фишечка"
    This reverts commit 3680f8b776756f6c33c4b2f0c72bf7804e8c8164.

--- support/static/support/js/index.js
+++ support/static/support/js/index.js
@@ -32,7 +32,6 @@ ws.onmessage = (event) => {
 // html events
 ticketAssignElement.addEventListener("click", () => {
     controller({ type: TICKETS_EVENTS.SEND.TICKET_ASSIGN }, ws);
-    btnSetMyTicketsElement.click();
 });
 
 sendMessageElement.addEventListener("click", () => {
