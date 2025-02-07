58463b306dc10b4b5765c3e20c183c0444528669
Rostislaww <rostiki.com@mail.ru>
2025-02-08 00:36:19 +0300

    лишнее убрал

--- support/examples/backend/my_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class MyUser(models.Model):
-    telegram_id = models.BigIntegerField(
-        primary_key=True,
-        unique=True,
-    )
